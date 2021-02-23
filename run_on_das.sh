83520910d6c276c5450f4d88652424f34cc9b033
Melle Meewis <melle.meewis@deus.ai>
2021-02-23 15:09:29 +0100

    run_on_das

---
+++ run_on_das.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#SBATCH --time=72:00:00
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=1
+#SBATCH -C TitanX
+#SBATCH --gres=gpu:1
+
+
+# Load GPU drivers
+module load cuda10.0/toolkit
+module load cuDNN/cuda10.0
+
+
+# This loads the anaconda virtual environment with our packages
+source /home/mms496/.bashrc
+
+
+# Base directory for the experiment
+cd /home/mms496/
+
+
+# Simple trick to create a unique directory for each run of the script
+
+echo $$
+mkdir o`echo $$`
+cd o`echo $$`
+
+
+# Run the actual experiment
+
+python -u /home/mms496/code/styleVAE.py
