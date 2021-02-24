da159fca942e59b4ec86437051829cd440279dbb
Melle Meewis <melle.meewis@deus.ai>
2021-02-24 14:25:20 +0100

    add dense

--- stylevae.py
+++ stylevae.py
@@ -423,8 +423,9 @@ def go(arg):
                 # -- decoding
                 xout = decoder(zsample, n0sample, n1sample, n2sample, n3sample, n4sample, n5sample)
 
-                dense_input = densenet(input)
-                dense_output = densenet(xout)
+                with torch.no_grad():
+                    dense_input = densenet(input)
+                    dense_output = densenet(xout)
 
                 # m = ds.Normal(xout[:, :C, :, :], xout[:, C:, :, :])
                 # rec_loss = - m.log_prob(target).sum(dim=1).sum(dim=1).sum(dim=1)
