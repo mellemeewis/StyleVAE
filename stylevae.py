c29ac833b81cba5438feb9dedee685e28c4b5579
Melle Meewis <melle.meewis@deus.ai>
2021-02-26 16:30:45 +0100

    minor

--- stylevae.py
+++ stylevae.py
@@ -393,13 +393,13 @@ def go(arg):
                 loss = perceptual_loss + br * rec_loss + bz * zkl + b0 * n0kl + b1 * n1kl + b2 * n2kl + b3 * n3kl + b4 * n4kl + b5 * n5kl
                 loss = loss.mean(dim=0)
 
-                if i%720 == 0:
-                    print("TTRAIN LOSSES: ")
-                    print('PER: ', perceptual_loss)
-                    print('REC: ', rec_loss)
-                    print("Z KL: ", zkl)
-                    print('NO-N5 KL: ', n0kl, n1kl, n2kl, n3kl, n4kl, n5kl)
-                    print('MEAN: ', loss)
+                # if i%720 == 0:
+                #     print("TTRAIN LOSSES: ")
+                #     print('PER: ', perceptual_loss)
+                #     print('REC: ', rec_loss)
+                #     print("Z KL: ", zkl)
+                #     print('NO-N5 KL: ', n0kl, n1kl, n2kl, n3kl, n4kl, n5kl)
+                #     print('MEAN: ', loss)
 
                 instances_seen += input.size(0)
 
@@ -475,12 +475,12 @@ def go(arg):
 
                             loss = perceptual_loss + rec_loss + zkl + n0kl + n1kl + n2kl + n3kl + n4kl + n5kl
                             loss = loss.mean(dim=0)
-                            print("LOSSES: ")
-                            print('PER: ', perceptual_loss)
-                            print('REC: ', rec_loss)
-                            print("Z KL: ", zkl)
-                            print('NO-N5 KL: ', n0kl, n1kl, n2kl, n3kl, n4kl, n5kl)
-                            print('MEAN: ', loss)
+                            # print("LOSSES: ")
+                            # print('PER: ', perceptual_loss)
+                            # print('REC: ', rec_loss)
+                            # print("Z KL: ", zkl)
+                            # print('NO-N5 KL: ', n0kl, n1kl, n2kl, n3kl, n4kl, n5kl)
+                            # print('MEAN: ', loss)
 
                             err_te.append(loss.data.item())
 
