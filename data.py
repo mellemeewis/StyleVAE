232eac3206ff1aa09abfc99d7def6be1295c67bc
Melle Meewis <melle.meewis@deus.ai>
2021-03-11 10:39:15 +0100

    EM

--- data.py
+++ data.py
@@ -15,72 +15,72 @@ def return_data(task, data_dir, batch_size):
 
 	
 	if task == 'mnist':
-        transform = Compose([ToTensor(), Pad(2, fill=0, padding_mode='constant')])
+		transform = Compose([ToTensor(), Pad(2, fill=0, padding_mode='constant')])
 
-        trainset = torchvision.datasets.MNIST(root=data_dir, train=True,
-                                                download=True, transform=transform)
-        trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
-                                                  shuffle=True, num_workers=2)
+		trainset = torchvision.datasets.MNIST(root=data_dir, train=True,
+		                                        download=True, transform=transform)
+		trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+		                                          shuffle=True, num_workers=2)
 
-        testset = torchvision.datasets.MNIST(root=data_dir, train=False,
-                                               download=True, transform=transform)
-        testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
-                                                 shuffle=False, num_workers=2)
+		testset = torchvision.datasets.MNIST(root=data_dir, train=False,
+		                                       download=True, transform=transform)
+		testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+		                                         shuffle=False, num_workers=2)
         C, H, W = 1, 32, 32
 
     elif task == 'cifar10':
-        trainset = torchvision.datasets.CIFAR10(root=data_dir, train=True,
-                                                download=True, transform=ToTensor())
-        trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
-                                                  shuffle=True, num_workers=2)
+		trainset = torchvision.datasets.CIFAR10(root=data_dir, train=True,
+		                                        download=True, transform=ToTensor())
+		trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+		                                          shuffle=True, num_workers=2)
 
-        testset = torchvision.datasets.CIFAR10(root=data_dir, train=False,
-                                               download=True, transform=ToTensor())
-        testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
-                                                 shuffle=False, num_workers=2)
-        C, H, W = 3, 32, 32
+		testset = torchvision.datasets.CIFAR10(root=data_dir, train=False,
+		                                       download=True, transform=ToTensor())
+		testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+		                                         shuffle=False, num_workers=2)
+		C, H, W = 3, 32, 32
 
     elif task == 'cifar-gs':
-        transform = Compose([Grayscale(), ToTensor()])
+		transform = Compose([Grayscale(), ToTensor()])
 
-        trainset = torchvision.datasets.CIFAR10(root=data_dir, train=True,
-                                                download=True, transform=transform)
-        trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
-                                                  shuffle=True, num_workers=2)
+		trainset = torchvision.datasets.CIFAR10(root=data_dir, train=True,
+		                                        download=True, transform=transform)
+		trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+		                                          shuffle=True, num_workers=2)
 
-        testset = torchvision.datasets.CIFAR10(root=data_dir, train=False,
-                                               download=True, transform=transform)
-        testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
-                                                 shuffle=False, num_workers=2)
-        C, H, W = 1, 32, 32
+		testset = torchvision.datasets.CIFAR10(root=data_dir, train=False,
+		                                       download=True, transform=transform)
+		testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+		                                         shuffle=False, num_workers=2)
+		C, H, W = 1, 32, 32
 
     elif task == 'imagenet64':
-        transform = Compose([ToTensor()])
+		transform = Compose([ToTensor()])
 
-        trainset = torchvision.datasets.ImageFolder(root=data_dir+os.sep+'train',
-                                                    transform=transform)
-        trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
-                                                  shuffle=True, num_workers=2)
+		trainset = torchvision.datasets.ImageFolder(root=data_dir+os.sep+'train',
+		                                            transform=transform)
+		trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+		                                          shuffle=True, num_workers=2)
 
-        testset = torchvision.datasets.ImageFolder(root=data_dir+os.sep+'valid',
-                                                   transform=transform)
-        testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
-                                                 shuffle=False, num_workers=2)
-        C, H, W = 3, 64, 64
+		testset = torchvision.datasets.ImageFolder(root=data_dir+os.sep+'valid',
+		                                           transform=transform)
+		testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+		                                         shuffle=False, num_workers=2)
+		C, H, W = 3, 64, 64
 
     elif task == 'ffhq':
-        tftrain = Compose([RandomHorizontalFlip(0.5), ToTensor()])
-        trainset = torchvision.datasets.ImageFolder(root=data_dir+os.sep+'train',
-                                                    transform=tftrain)
-        trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
-                                                  shuffle=True, num_workers=2)
+		tftrain = Compose([RandomHorizontalFlip(0.5), ToTensor()])
+		trainset = torchvision.datasets.ImageFolder(root=data_dir+os.sep+'train',
+		                                            transform=tftrain)
+		trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+		                                          shuffle=True, num_workers=2)
 
-        tftest = Compose([ToTensor()])
-        testset = torchvision.datasets.ImageFolder(root=data_dir+os.sep+'valid',
-                                                   transform=tftest)
-        testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
-                                                 shuffle=False, num_workers=2)
-        C, H, W = 3, 128, 128
+		tftest = Compose([ToTensor()])
+		testset = torchvision.datasets.ImageFolder(root=data_dir+os.sep+'valid',
+		                                           transform=tftest)
+		testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+		                                         shuffle=False, num_workers=2)
+		C, H, W = 3, 128, 128
 
     else:
         raise Exception('Task {} not recognized.'.format(task))
