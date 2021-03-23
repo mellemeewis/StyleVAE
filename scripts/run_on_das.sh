ebed5157a671897e6aa4131cf93734b750503558
Melle Meewis <melle.meewis@deus.ai>
2021-03-23 16:48:26 +0100

    minor

--- scripts/run_on_das.sh
+++ scripts/run_on_das.sh
@@ -17,7 +17,7 @@ source /home/mms496/.bashrc
 
 
 # Base directory for the experiment
-cd /home/mms496/StyleVAE_Experiments
+cd /home/mms496/StyleVAE_Experiments/partial
 
 # Simple trick to create a unique directory for each run of the script
 
@@ -28,4 +28,14 @@ cd o`echo $$`
 
 # Run the actual experiment
 
-python -u /home/mms496/StyleVAE_Experiments/code/StyleVAE/stylevae.py --task ffhq --numplots 10 -z 256 -e 1 1 1 1 1 1000 -l 0.00005 -b 32 --betas 0.6 1 1 1 1 1 1 1 --dropouts 0.01 0.01 0.01 0.01 0.01 0.01 0.01 --mapping-layers 6 -D /var/scratch/mms496/data/ffhq/partial/thumbnails128x128
+python -u /home/mms496/StyleVAE_Experiments/code/StyleVAE/stylevae.py --task ffhq --numplots 10 -z 256 -e 1 0 0 0 0 0 -l 0.00005 -b 32 --betas 0.6 1 1 1 1 1 1 1 --dropouts 0.01 0.01 0.01 0.01 0.01 0.01 0.01 --mapping-layers 6 -D /var/scratch/mms496/data/ffhq/partial/thumbnails128x128
+
+cd /home/mms496/StyleVAE_Experiments/full
+
+# Simple trick to create a unique directory for each run of the script
+
+echo $$
+mkdir o`echo $$`
+cd o`echo $$`
+
+python -u /home/mms496/StyleVAE_Experiments/code/StyleVAE/stylevae.py --task ffhq --numplots 10 -z 256 -e 1 0 0 0 0 0 -l 0.00005 -b 32 --betas 0.6 1 1 1 1 1 1 1 --dropouts 0.01 0.01 0.01 0.01 0.01 0.01 0.01 --mapping-layers 6 -D /var/scratch/mms496/data/ffhq/full/thumbnails128x128
