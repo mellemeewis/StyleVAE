9d9d1a8ea4fc7d2c2990f7de72aec87fe9ca671d
Melle Meewis <melle.meewis@deus.ai>
2021-03-23 15:41:48 +0100

    minor

--- scripts/run_on_das.sh
+++ scripts/run_on_das.sh
@@ -29,4 +29,4 @@ cd o`echo $$`
 
 # Run the actual experiment
 
-python -u /home/mms496/code/StyleVAE/stylevae.py --task ffhq --numplots 5 -z 256 -e 1 1 1 1 1 1 -l 0.00005 -b 32 --betas 0.6 1 1 1 1 1 1 1 --dropouts 0.01 0.01 0.01 0.01 0.01 0.01 0.01 --mapping-layers 6 -D /var/scratch/mms496/data/ffhq/partial/thumbnails128x128
+python -u /home/mms496/StyleVAE_Experiments/code/StyleVAE/stylevae.py --task ffhq --numplots 5 -z 256 -e 1 1 1 1 1 1 -l 0.00005 -b 32 --betas 0.6 1 1 1 1 1 1 1 --dropouts 0.01 0.01 0.01 0.01 0.01 0.01 0.01 --mapping-layers 6 -D /var/scratch/mms496/data/ffhq/partial/thumbnails128x128
