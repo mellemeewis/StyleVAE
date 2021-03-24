14f241a15857f569c0e688b4e5ea1b8328a971bb
Melle Meewis <melle.meewis@deus.ai>
2021-03-24 16:08:56 +0100

    minor

---
+++ scripts/run_f_on_das.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#SBATCH --time=12:00:00
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=1
+#SBATCH -C TitanX
+#SBATCH --gres=gpu:1
+
+
+# Load GPU drivers
+module load cuda10.0/toolkit
+module load cuDNN/cuda10.0
+
+
+# This loads the anaconda virtual environment with our packages
+source /home/mms496/.bashrc
+
+
+# Base directory for the experiment
+cd /home/mms496/StyleVAE_Experiments/full
+
+# Simple trick to create a unique directory for each run of the script
+
+echo $$
+mkdir o`echo $$`
+cd o`echo $$`
+
+
+# Run the actual experiment
+
+python -u /home/mms496/StyleVAE_Experiments/code/StyleVAE/stylevae.py --task ffhq --numplots 30 -z 256 -e 1 1 1 1 1 1 -l 0.0001 -b 32 --betas 0.4 1 1 1 1 1 1 1 --dropouts 0.01 0.02 0.02 0.02 0.02 0.02 0.02 --mapping-layers 6 -D /var/scratch/mms496/data/ffhq/full/thumbnails128x128
+
