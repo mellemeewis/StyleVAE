7fd614f5c61d5271cca714d3f3a36b87974f4c05
Melle Meewis <melle.meewis@deus.ai>
2021-03-31 12:19:01 +0200

    minor

--- stylevae.py
+++ stylevae.py
@@ -595,13 +595,13 @@ def go(arg):
                     n5sample = util.sample_image(n5)
 
                     # -- decoding
-                    xout = util.batchedn((zsample, n0sample, n1sample, n2sample, n3sample, n4sample, n5sample), decoder, batch_size=4).clamp(0, 1)[:, :C, :, :]
+                    xout = util.batchedn((zsample, n0sample, n1sample, n2sample, n3sample, n4sample, n5sample), decoder, batch_size=4).clamp(0, 1)[:, :2*C, :, :]
                     xout = util.sample_image(xout)
                     # -- mix the latent vector with random noise
-                    mixout = util.batchedn((zsample, n0rand, n1rand, n2rand, n3rand, n4rand, n5rand), decoder, batch_size=4).clamp(0, 1)[:, :C, :, :]
+                    mixout = util.batchedn((zsample, n0rand, n1rand, n2rand, n3rand, n4rand, n5rand), decoder, batch_size=4).clamp(0, 1)[:, :2*C, :, :]
                     mixout = util.sample_image(mixout)
                     # -- mix a random vector with the sample noise
-                    mixout2 = util.batchedn((zrand, n0sample, n1sample, n2sample, n3sample, n4sample, n5sample), decoder, batch_size=4).clamp(0, 1)[:, :C, :, :]
+                    mixout2 = util.batchedn((zrand, n0sample, n1sample, n2sample, n3sample, n4sample, n5sample), decoder, batch_size=4).clamp(0, 1)[:, :2*C, :, :]
                     mixout2 = util.sample_image(mixout2)
 
                     images = torch.cat([input.cpu()[:24,:,:], xout[:24,:,:], mixout[:24,:,:], mixout2[:24,:,:], sample[:24,:,:],
