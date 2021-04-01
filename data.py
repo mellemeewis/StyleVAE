a805c3a5943f1dbaf662a9b5848f97c7e2cffbee
Melle Meewis <melle.meewis@deus.ai>
2021-04-01 11:16:18 +0200

    minor

--- data.py
+++ data.py
@@ -16,14 +16,14 @@ def return_data(task, data_dir, batch_size):
 
 
 	if task == 'mnist':
-		transform = Compose([ToPILImage(), ToTensor(), Pad(2, fill=0, padding_mode='constant')])
+		transform = Compose([ToTensor(), Pad(2, fill=0, padding_mode='constant'), Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
 
-		trainset = torchvision.datasets.QMNIST(root=data_dir, train=True,
+		trainset = torchvision.datasets.MNIST(root=data_dir, train=True,
 		                                        download=True, transform=transform)
 		trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
 		                                          shuffle=True, num_workers=2)
 
-		testset = torchvision.datasets.QMNIST(root=data_dir, train=False,
+		testset = torchvision.datasets.MNIST(root=data_dir, train=False,
 		                                       download=True, transform=transform)
 		testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
 		                                         shuffle=False, num_workers=2)
