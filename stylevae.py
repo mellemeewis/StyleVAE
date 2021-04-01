24e3badc5f1335456f75f230fa181376fc371309
Melle Meewis <melle.meewis@deus.ai>
2021-04-01 15:16:27 +0200

    minor

--- stylevae.py
+++ stylevae.py
@@ -106,8 +106,18 @@ class StyleEncoder(nn.Module):
         z1 = self.affine1(x1.view(b, -1))
         n1 = self.tonoise1(x1)
 
+        assert torch.isnan(x1).sum() == 0
+        assert torch.isnan(z1).sum() == 0
+        assert torch.isnan(n1).sum() == 0
+        assert torch.isinf(x1).sum() == 0
+        assert torch.isinf(z1).sum() == 0
+        assert torch.isinf(n1).sum() == 0
+        assert torch.isnan(z0).sum() == 0
+        assert torch.isinf(z0).sum() == 0
         if depth <= 1:
             z = self.unmapping(z0 + z1)
+            assert torch.isnan(z).sum() == 0
+            assert torch.isinf(z).sum() == 0
             return z, n0, n1, n2, n3, n4, n5
 
         x2 = F.avg_pool2d(self.block2(x1), 2)
@@ -434,11 +444,15 @@ def go(arg):
 
 
                     isample = util.sample_image(i)
-                    if torch.isnan(isample).sum() > 0:
-                            print(f'isample contains nan')
-                    if torch.isinf(isample).sum() > 0:
-                            print(f'isample contains inf')
-                            
+
+
+                    for n, p in encoder.named_parameters():
+                        if torch.isnan(p).sum() > 0:
+                            print(f'{n} contains nan')
+                        if torch.isinf(p).sum() > 0:
+                            print(f'{n} contains inf')
+
+
                     iz, in0, in1, in2, in3, in4, in5 = encoder(isample, depth)
 
                     iz_loss = util.normal_lt_loss(iz, zrand).mean()
@@ -504,12 +518,7 @@ def go(arg):
                     opte.step()
                     opte.zero_grad()
 
-                    for n, p in encoder.named_parameters():
-                        if torch.isnan(p).sum() > 0:
-                            print(f'{n} contains nan')
-                        if torch.isinf(p).sum() > 0:
-                            print(f'{n} contains inf')
-
+                    
                 # optimizer.zero_grad()
 
                 # loss.backward()
