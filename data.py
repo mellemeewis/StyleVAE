e0e3817a9c9c77bc4c89133f61d8c984dacc71bc
Melle Meewis <melle.meewis@deus.ai>
2021-04-05 18:49:22 +0200

    minor

--- data.py
+++ data.py
@@ -98,6 +98,20 @@ def return_data(task, data_dir, batch_size):
 		C, H, W = 3, 64, 64
 
 	elif task == 'ffhq':
+		tftrain = Compose([RandomHorizontalFlip(0.5), ToTensor()])
+		trainset = torchvision.datasets.ImageFolder(root=data_dir,
+		                                            transform=tftrain)
+		trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+		                                          shuffle=True, num_workers=2)
+
+		tftest = Compose([ToTensor()])
+		testset = torchvision.datasets.ImageFolder(root=data_dir+os.sep,
+		                                           transform=tftest)
+		testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+		                                         shuffle=False, num_workers=2)
+		C, H, W = 3, 128, 128
+
+	elif task == 'ffhq-gs':
 		tftrain = Compose([Grayscale(), RandomHorizontalFlip(0.5), ToTensor()])
 		trainset = torchvision.datasets.ImageFolder(root=data_dir,
 		                                            transform=tftrain)
@@ -109,8 +123,7 @@ def return_data(task, data_dir, batch_size):
 		                                           transform=tftest)
 		testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
 		                                         shuffle=False, num_workers=2)
-		C, H, W = 3, 128, 128
-
+		C, H, W = 1, 128, 128
 	else:
 		raise Exception('Task {} not recognized.'.format(task))
 
