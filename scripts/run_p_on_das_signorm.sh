41c7787c71f2c65e97f814c6ea4af2b0bc3ffdff
Melle Meewis <MelleMeewis@MacBook-Pro-van-Pepijn.local>
2021-04-08 17:33:31 +0200

    minor

--- scripts/run_p_on_das_signorm.sh
+++ scripts/run_p_on_das_signorm.sh
@@ -28,4 +28,4 @@ cd o`echo $$`
 
 # Run the actual experiment
 
-python -u /home/mms496/StyleVAE_Experiments/code/StyleVAE/stylevae.py  --task ffhq-gs --numplots 1000 -z 512 -e 0 0 0 0 0 1 -l 0.0001 -b 256 --betas 1 1 0 0 0 0 0 0 --dropouts 0 0 0 0 0 0 0 --mapping-layers 3 -EU 1 --skip-test -EN 2 -DE 2 -L signorm -D /var/scratch/mms496/data/ffhq/partial/thumbnails128x128 # --channels 64 128 256 512 1024 --zchannels 4 8 16 32 64 128 
+python -u /home/mms496/StyleVAE_Experiments/code/StyleVAE/stylevae.py  --task ffhq-gs --numplots 1000 -z 512 -e 0 0 0 0 0 1 -l 0.0001 -b 128 --betas 1 1 0 0 0 0 0 0 --dropouts 0 0 0 0 0 0 0 --mapping-layers 3 -EU 1 --skip-test -EN 2 -DE 2 -L signorm -D /var/scratch/mms496/data/ffhq/partial/thumbnails128x128 # --channels 64 128 256 512 1024 --zchannels 4 8 16 32 64 128 
