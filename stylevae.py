39f6d8f1dc3fb72c43005c143eda5d340833eb17
Melle Meewis <MelleMeewis@MacBook-Pro-van-Pepijn.local>
2021-04-13 12:09:47 +0200

    minor

--- stylevae.py
+++ stylevae.py
@@ -127,7 +127,7 @@ def go(arg):
                 z_prime = encoder(xsample, depth)
 
                 # -- compute loss
-                sleep_loss = util.sleep_loss(z_prime, zrand).
+                sleep_loss = util.sleep_loss(z_prime, zrand)
                 print(sleep_loss.size())
                 sleep_loss = sleep_loss.mean()
                 print(sleep_loss.size())
