b851f8062c8bd78c3904ad4f90c5e989093def39
Melle Meewis <MelleMeewis@MacBook-Pro-van-Pepijn.local>
2021-04-13 14:50:19 +0200

    minor

--- util.py
+++ util.py
@@ -262,7 +262,7 @@ def sample_images(z, distribution, n=1, eps=None):
         assert torch.isinf(scale).sum() == 0
         assert scale[scale < 0].sum() == 0
 
-        scale = torch.clamp(min=0.0001)
+        scale = torch.clamp(scale, min=0.0001)
         print(scale)
         distribution = torch.distributions.laplace.Laplace(loc, scale, validate_args=None)
         sample = distribution.sample(sample_shape=(n,))
