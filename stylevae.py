30b4b0877fa1cc055ea69ea37ee0a8c32f8c8a07
Melle Meewis <MelleMeewis@MacBook-Pro-van-Pepijn.local>
2021-04-14 09:24:53 +0200

    minor

--- stylevae.py
+++ stylevae.py
@@ -113,6 +113,8 @@ def go(arg):
                 rec_loss = rec_loss.sum(dim=1)
                 print('SUM ', rec_loss.size(), rec_loss)            
                 kl_loss  = util.kl_loss(z[:, :zs], z[:, zs:])
+                print("KL ", kl_loss.size())
+                print(kl_loss)
                 loss = br*rec_loss + bz * kl_loss
                 loss = loss.mean(dim=0)
 
