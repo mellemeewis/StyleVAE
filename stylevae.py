fa77f08d1bf4d62ca5d8d83a8520e3b5f4b6785f
Melle Meewis <MelleMeewis@MacBook-Pro-van-Pepijn.local>
2021-04-15 12:56:54 +0200

    minor

--- stylevae.py
+++ stylevae.py
@@ -45,7 +45,7 @@ def go(arg):
 
     br, bz, b0, b1, b2, b3, b4, b5, bs = arg.betas
     bz_list = torch.arange(0, bz, step = bz / torch.sum(torch.tensor(arg.epochs)))
-    print(bz_list)
+    print(bz_list.size())
     C, H, W, trainset, trainloader, testset, testloader = return_data(arg.task, arg.data_dir, arg.batch_size)
     zs = arg.latent_size
 
@@ -81,6 +81,7 @@ def go(arg):
         
         for epoch in range(arg.epochs[depth]):
 
+            bz = bz_list[epoch]
             epoch_loss = [0,0,0,0,0,0,0,0,0,0]
 
 
