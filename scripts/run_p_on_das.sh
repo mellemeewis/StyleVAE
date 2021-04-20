ce57e27ac34bf9fa600fa3030a15d4e1384d42bd
Melle Meewis <MelleMeewis@MacBook-Pro-van-Pepijn.local>
2021-04-20 15:22:47 +0200

    minor

--- scripts/run_p_on_das.sh
+++ scripts/run_p_on_das.sh
@@ -28,4 +28,4 @@ cd o`echo $$`
 
 # Run the actual experiment
 
-python -u /home/mms496/StyleVAE_Experiments/code/StyleVAE/stylevae.py  --task ffhq-gs --numplots 50 -z 512 -e 0 0 0 0 0 1000 -l 0.0001 0.0001 -b 32 40 0.4 0 0 0 0 0 0 --skip-test --train-recon-with-rn 30 -D /var/scratch/mms496/data/ffhq/partial/thumbnails128x128 # --channels 64 128 256 512 1024 --zchannels 4 8 16 32 64 128 
+python -u /home/mms496/StyleVAE_Experiments/code/StyleVAE/stylevae.py  --task ffhq-gs --numplots 50 -z 512 -e 0 0 0 0 0 1000 -l 0.0001 0.0001 -b 32 --betas 40 0.4 0 0 0 0 0 0 --skip-test --train-recon-with-rn 30 -D /var/scratch/mms496/data/ffhq/partial/thumbnails128x128 # --channels 64 128 256 512 1024 --zchannels 4 8 16 32 64 128 
