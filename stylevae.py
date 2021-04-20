2d6676318b19b0c41c1f2e4c43ae43ec2ab38b7b
Melle Meewis <MelleMeewis@MacBook-Pro-van-Pepijn.local>
2021-04-20 12:16:31 +0200

    minor

--- stylevae.py
+++ stylevae.py
@@ -172,6 +172,7 @@ def go(arg):
                     #disc updates, so second pass trough disc.
                 discriminator_out_fake = discriminator(xout[:, :C, :, :]).view(-1)
                 generator_loss = discriminator_criterion(discriminator_out_fake, real_label)
+                print(generator_loss)
 
 
 
